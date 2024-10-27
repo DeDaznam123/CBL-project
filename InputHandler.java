4c2102ff1af3e181dff01cccd8137cbca4d466c7
xacobeu <miguelartlebrun@gmail.com>
2024-10-27 17:36:28 +0100

    Volume Slider.

---
+++ InputHandler.java
@@ -0,0 +1,98 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+/**
+ * Handles the keyboard input from the player.
+ */
+public class InputHandler implements KeyListener, MouseListener {
+    public boolean mouseClicked = false;
+
+    public boolean wPressed;
+    public boolean sPressed;
+    public boolean aPressed;
+    public boolean dPressed;
+    public boolean escPressed;
+
+    public boolean escHandled;
+
+    @Override
+    public void keyPressed(KeyEvent arg0) {
+        int key = arg0.getKeyCode();
+
+        switch (key) {
+            case KeyEvent.VK_W:
+                wPressed = true;
+                break;
+            case KeyEvent.VK_S:
+                sPressed = true;
+                break;
+            case KeyEvent.VK_A:
+                aPressed = true;
+                break;
+            case KeyEvent.VK_D:
+                dPressed = true;
+                break;
+            case KeyEvent.VK_ESCAPE:
+                if (!escHandled) {
+                    escPressed = true;
+                    escHandled = true;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent arg0) {
+        int key = arg0.getKeyCode();
+
+        switch (key) {
+            case KeyEvent.VK_W:
+                wPressed = false;
+                break;
+            case KeyEvent.VK_S:
+                sPressed = false;
+                break;
+            case KeyEvent.VK_A:
+                aPressed = false;
+                break;
+            case KeyEvent.VK_D:
+                dPressed = false;
+                break;
+            case KeyEvent.VK_ESCAPE:
+                escPressed = false;
+                escHandled = false;
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent arg0) { }
+
+    
+    // Mouse related events
+
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        mouseClicked = true;
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        mouseClicked = false;
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) { }
+
+    @Override
+    public void mouseExited(MouseEvent e) { }
+}
